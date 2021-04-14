47528a844b28820db059fd4c5d2f4d3d4e180e12
pinooxd <pinooxd@gmail.com>
2021-04-13 17:04:29 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', () => {
+    let lon;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            lon = position.coords.longitude.toFixed(2);
+            lat = position.coords.latitude.toFixed(2);
+         //   const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=eb04835dda9d8a175d43f9cc3c5890b5`;
+            
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temp} = data.main;
+                const [{description}] = data.weather;
+                const zoneName = data.name;
+                const celsius = temp-273.15;
+                //Set DOM Element from the API
+                                
+                locationTimezone.textContent = zoneName;
+                temperatureDescription.textContent = description;
+                temperatureDegree.textContent = celsius;
+                
+            });
+        });
+        
+    } 
+});
