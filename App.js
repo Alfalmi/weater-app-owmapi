f533c22c067223462dee792de1c49fb6574f8577
pinooxd <pinooxd@gmail.com>
2021-04-13 18:43:31 -0700

    plop

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             lon = position.coords.longitude.toFixed(2);
             lat = position.coords.latitude.toFixed(2);
-         //   const proxy = 'https://cors-anywhere.herokuapp.com/';
+           // const proxy = 'https://cors-anywhere.herokuapp.com/';
             const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=eb04835dda9d8a175d43f9cc3c5890b5`;
             
             fetch(api)
@@ -24,7 +24,8 @@ window.addEventListener('load', () => {
                 const zoneName = data.name;
                 const celsius = temp-273.15;
                 //Set DOM Element from the API
-                                
+                
+                
                 locationTimezone.textContent = zoneName;
                 temperatureDescription.textContent = description;
                 temperatureDegree.textContent = celsius;
