5af6bf474008336892210a6dc8e99803069d5748
pinooxd <pinooxd@gmail.com>
2021-04-13 19:49:30 -0700

    working

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let locationIcon = document.querySelector('.weather-icon');
     
 
     if(navigator.geolocation) {
@@ -20,16 +21,18 @@ window.addEventListener('load', () => {
             .then(data => {
                 console.log(data);
                 const {temp} = data.main;
-                const [{description}] = data.weather;
+                const [{description, icon}] = data.weather;
                 const zoneName = data.name;
-                const celsius = temp-273.15;
+                const celsius = (temp-273.15).toFixed(1);
                 //Set DOM Element from the API
-                
+                console.log(celsius);
+
                 
                 locationTimezone.textContent = zoneName;
                 temperatureDescription.textContent = description;
                 temperatureDegree.textContent = celsius;
                 
+                locationIcon.innerHTML = `<img src="icons/${icon}.png"></img>`;
             });
         });
         
